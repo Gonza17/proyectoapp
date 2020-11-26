d215a39b1b6734446bc1fac846474578f8eba571
Gonza17 <gonza.a.jaques17@gmail.com>
2020-11-26 00:28:46 -0300

    pagina principal

--- proyectoapp/lib/home.dart
+++ proyectoapp/lib/home.dart
@@ -1,26 +1,40 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Nombre'),
+        title: Text('King´s Food'),
       ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            ListTile(
-              title: Text('Opciones'),
-              onTap: () {},
-            ),
-            ListTile(
-              title: Text('guardado'),
-              onTap: () {},
-            )
-          ],
-        ),
-      ),
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('inicio'),
+            backgroundColor: Colors.orange[300]),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text('Buscar'),
+            backgroundColor: Colors.orange[300]),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.camera),
+            title: Text('nuevo post'),
+            backgroundColor: Colors.orange[300]),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            title: Text('Mensajes'),
+            backgroundColor: Colors.orange[300]),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('perfil'),
+            backgroundColor: Colors.orange[300]),
+      ]),
     );
   }
 }
