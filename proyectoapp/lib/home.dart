1a1c2403dcbdf1a93bc072e295c2a320e5204254
Gonza17 <gonza.a.jaques17@gmail.com>
2020-11-26 01:05:31 -0300

    Update home.dart

--- proyectoapp/lib/home.dart
+++ proyectoapp/lib/home.dart
@@ -6,35 +6,53 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+
+  final tabs = [
+    Center(child: Text('home')),
+    Center(child: Text('buscar')),
+    Center(child: Text('new post')),
+    Center(child: Text('mensajes')),
+    Center(child: Text('perfil')),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('King´s Food'),
       ),
-      body: Container(),
-      bottomNavigationBar: BottomNavigationBar(currentIndex: 0, items: [
-        BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'inicio',
-            backgroundColor: Colors.red),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'buscar',
-            backgroundColor: Colors.red),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.camera),
-            label: 'nuevo post',
-            backgroundColor: Colors.red),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.message),
-            label: 'Mensajes',
-            backgroundColor: Colors.red),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'perfil',
-            backgroundColor: Colors.red),
-      ]),
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'inicio',
+              backgroundColor: Colors.blue),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'buscar',
+              backgroundColor: Colors.red),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.camera),
+              label: 'nuevo post',
+              backgroundColor: Colors.orange),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              label: 'Mensajes',
+              backgroundColor: Colors.yellow),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'perfil',
+              backgroundColor: Colors.green),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
     );
   }
 }
