bfadb7f6bb69b1b2b0519e739f7d705afa02ccae
Gonza17 <gonza.a.jaques17@gmail.com>
2020-12-13 19:28:09 -0300

    new post

--- proyectoapp/lib/newpost.dart
+++ proyectoapp/lib/newpost.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:image_picker/image_picker.dart';
 
 class Newpost extends StatelessWidget {
+  File _image;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    var PickedFile = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (PickedFile != null) {
+        _image = image;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: new Text("aqui va el newpost"));
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Image Picker Example'),
+      ),
+      body: new Center(
+        child: _image == null
+            ? new Text('no image selected')
+            : new Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
   }
 }
