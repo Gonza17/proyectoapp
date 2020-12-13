e36649046c799804d5ea3926f8ec5786b6e4dbe7
Gonza17 <gonza.a.jaques17@gmail.com>
2020-12-13 17:17:31 -0300

    Update newpost.dart

--- proyectoapp/lib/newpost.dart
+++ proyectoapp/lib/newpost.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class Newpost extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(child: new Text("aqui va el inicio"));
+    return Container(child: new Text("aqui va el newpost"));
   }
 }
