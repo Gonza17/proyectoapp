5bc588ffdd49566ea74a323eac35b2592238ca0b
Gonza17 <gonza.a.jaques17@gmail.com>
2020-12-13 22:24:45 -0300

    newpost

--- proyectoapp/lib/newpost.dart
+++ proyectoapp/lib/newpost.dart
@@ -1,8 +1,112 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
 
-class Newpost extends StatelessWidget {
+class Newpost extends StatefulWidget {
+  _Newpost createState() => new _Newpost();
+}
+
+enum AppState {
+  free,
+  picked,
+  cropped,
+}
+
+class _Newpost extends State<Newpost> {
+  AppState state;
+  File image;
+  @override
+  void initState() {
+    super.initState();
+    state = AppState.free;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: new Text("aqui van los nuevos post"));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("experimento"),
+      ),
+      body: Center(
+        child: image != null ? Image.file(image) : Container(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange,
+        onPressed: () {
+          if (state == AppState.free)
+            _pickImage();
+          else if (state == AppState.picked)
+            _cropImage();
+          else if (state == AppState.cropped) _clearImage();
+        },
+        child: _buildButtonIcon(),
+      ),
+    );
+  }
+
+  Widget _buildButtonIcon() {
+    if (state == AppState.free)
+      return Icon(Icons.add);
+    else if (state == AppState.picked)
+      return Icon(Icons.crop);
+    else if (state == AppState.cropped)
+      return Icon(Icons.clear);
+    else
+      return Container();
+  }
+
+  Future _pickImage() async {
+    image = await ImagePicker.pickImage(source: ImageSource.camera);
+    if (image != null) {
+      setState(() {
+        state = AppState.picked;
+      });
+    }
+  }
+
+  Future _cropImage() async {
+    File croppedFile = await ImageCropper.cropImage(
+        sourcePath: image.path,
+        aspectRatioPresets: Platform.isAndroid
+            ? [
+                CropAspectRatioPreset.square,
+                CropAspectRatioPreset.ratio3x2,
+                CropAspectRatioPreset.original,
+                CropAspectRatioPreset.ratio4x3,
+                CropAspectRatioPreset.ratio16x9
+              ]
+            : [
+                CropAspectRatioPreset.original,
+                CropAspectRatioPreset.square,
+                CropAspectRatioPreset.ratio3x2,
+                CropAspectRatioPreset.ratio4x3,
+                CropAspectRatioPreset.ratio5x3,
+                CropAspectRatioPreset.ratio5x4,
+                CropAspectRatioPreset.ratio7x5,
+                CropAspectRatioPreset.ratio16x9
+              ],
+        androidUiSettings: AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: Colors.deepOrange,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        iosUiSettings: IOSUiSettings(
+          title: 'Cropper',
+        ));
+    if (croppedFile != null) {
+      image = croppedFile;
+      setState(() {
+        state = AppState.cropped;
+      });
+    }
+  }
+
+  void _clearImage() {
+    image = null;
+    setState(() {
+      state = AppState.free;
+    });
   }
 }
