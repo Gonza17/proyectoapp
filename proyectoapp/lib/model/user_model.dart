99ea36165b198af25549fe93888285de5b1089f0
Tomasespinozagarrido <49970658+Tomasespinozagarrido@users.noreply.github.com>
2020-12-23 02:16:34 -0300

    Create user_model.dart

---
+++ proyectoapp/lib/model/user_model.dart
@@ -0,0 +1,10 @@
+class Usuario {
+  Usuario ({this.uid,this.foto,this.ciudad,this.email,this.nombre,this.password});
+  String uid;
+  String foto;
+  String ciudad;
+  String email;
+  String nombre;
+  String password;
+  
+}
