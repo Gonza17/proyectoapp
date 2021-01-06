a71332295e685ba115f55b07dfd6556ae22142fe
Gonza17 <gonza.a.jaques17@gmail.com>
2021-01-05 22:21:09 -0300

    documentacion de las recetas

--- proyectoapp/lib/model/recetas.dart
+++ proyectoapp/lib/model/recetas.dart
@@ -1,3 +1,5 @@
+/* en el modelo de la receta se ocupara para poder recuperar los datos dentro del inicio 
+y  nos da la posibilidad de poder  manejar los datos dentro de la bd. */
 class Receta {
   Receta(
       {this.uid,
